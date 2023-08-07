b11b8b5f1e96c8317dd48ba207c7e632b017b6e7
LitavrinaAA <a.a.litavrina@google.com>
2023-08-07 18:47:03 +0300

    Second Num

--- example018/Program.cs
+++ example018/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+int InputN()
+{
+    System.Console.WriteLine("Введите трехзначное число, а я покажу его вторую цифру");
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+//check
+void FindSecond(int num){
+if ((num/100 > 0) && (num/1000 == 0)){
+    //System.Console.WriteLine("трехзначное");
+    
+    System.Console.WriteLine();
+    System.Console.WriteLine((num/10)%10);
+}
+else
+{
+    System.Console.WriteLine("не трехзначное");
+}
+}
+int n = InputN();
+FindSecond(n);
